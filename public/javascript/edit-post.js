d4f7ff12e8f8372f09ddcc53c38440e87d5f6e85
Kaijam Jeffers <faceless128@gmail.com>
2022-08-14 21:41:49 -0400

    updated edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,29 +1,29 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-    const post_title = document.querySelector('input[name="post-title"]').value.trim();
-    const post_data = document.querySelector('textarea[name="post-data"]').value.trim();
+  event.preventDefault();
+  const post_title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_data = document.querySelector('textarea[name="post-data"]').value.trim();
 
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-    if (post_title) {
-        const response = await fetch('/api/posts/' + post_id , {
-            method: 'PUT',
-            body: JSON.stringify({
-                title: post_title,
-                post_data: post_data
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+  const post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  if (post_title) {
+    const response = await fetch('/api/posts/' + post_id , {
+      method: 'PUT',
+      body: JSON.stringify({
+        title: post_title,
+        post_data: post_data
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
